c4607b7d81ae759e15f5c297c7ab2eb150b4d55c
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-03-21 09:39:09 +0000

    dpley2

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,7 +18,7 @@ export const Button = ({
   const checkButtonSize = SIZES.includes(buttonSize)
   ? buttonSize
   : SIZES[0];
-
+           
   return (
     <Link to='/contact' className='btn-mobile'>
       <button
